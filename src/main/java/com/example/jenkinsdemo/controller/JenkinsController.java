1021bc4757228e135fd6eddabd37aa3d9a42d672
admin <gerry123>
2020-06-30 16:50:50 +0800

    更改JenkinsController内容5

--- src/main/java/com/example/jenkinsdemo/controller/JenkinsController.java
+++ src/main/java/com/example/jenkinsdemo/controller/JenkinsController.java
@@ -12,6 +12,6 @@ public class JenkinsController {
 
     @RequestMapping("jenkins1")
     public String hello1() {
-        return "Hello Jenkins!~~~~";
+        return "Hello Jenkins!~~~111~";
     }
 }
